990214cde8f5f61d0ee8d7616e746fd063d3bfce
Aaron Fischer <aaron@aaronfischer.rf.lan>
2015-11-25 05:40:37 -0800

    .

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,27 +3,27 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user = User.from_omniauth(request.env["omniauth.auth"])
         @user.skip_confirmation!
 
-        if @user.persisted?
+        #if @user.persisted?
             # TODO reinstate or delete bootstrap-level flash/banner message for successful sign-in
             # flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
             sign_in_and_redirect @user, :event => :authentication
-        else
-            session["devise.google_data"] = request.env["omniauth.auth"]
-            redirect_to new_user_registration_url
-        end
+        #else
+        #    session["devise.google_data"] = request.env["omniauth.auth"]
+        #    redirect_to new_user_registration_url
+       # end
     end
 
     def facebook
         @user = User.from_omniauth(request.env["omniauth.auth"])
         @user.skip_confirmation!
 
-        if @user.persisted?
+      #  if @user.persisted?
             # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
             # set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
             sign_in_and_redirect @user, :event => :authentication
-        else
-            session["devise.facebook_data"] = request.env["omniauth.auth"]
-            redirect_to new_user_registration_url
-        end
+      #  else
+      #      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      #      redirect_to new_user_registration_url
+      #  end
     end
 end
